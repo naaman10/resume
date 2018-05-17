05c683ddddad135b034b69f074afdde0b4ec87d7
Super Break <superbreak@Naamans-MacBook-Air.local>
2018-05-17 23:48:10 +0100

    new updates

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,21 @@
-// smoothscroll
+$(document).ready(function (){
+  $(".nav-link").click(function(){
+    var navID = $(this).attr('href');
+    $('html, body').animate({
+        scrollTop: $( navID ).offset().top
+    }, 2000);
+  });
+});
 
+$("#footerNav").hide();
+$(window).scroll(function() {
+  var scroll = $(window).scrollTop();
+  if (scroll > 600) {
+    $('#footerNav').fadeIn();
+    console.log( scroll );
+  } else {
+    $("#footerNav").fadeOut();
+  };
+});
 
-// menuclose
+//600
